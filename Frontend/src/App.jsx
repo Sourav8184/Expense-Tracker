d73acf65767a592f7a614a695d0099807cbfcaa9
Sourav <ksourav662@gmail.com>
2024-06-05 19:18:27 +0530

    add Navigation Component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import "./App.scss";
 import BgAnimation from "./components/bgAnimation/BgAnimation";
+import Navigation from "./components/navigation/Navigation";
 function App() {
+  const [active, setActive] = useState(0);
   return (
     <div className="app-container">
       <BgAnimation />
-      <main className="app-main">Hello</main>
+      <main className="app-main">
+        <Navigation active={active} setActive={setActive} />
+      </main>
     </div>
   );
 }
--- Frontend/src/components/navigation/Navigation.jsx
+++ Frontend/src/components/navigation/Navigation.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import avatar from "../../images/avatar.png";
+import { menuItems } from "../../utils/menuItems";
+import { signout } from "../../utils/Icons";
 import "./Navigation.scss";
-function Navigation() {
-  return <div>Navigation</div>;
+
+function Navigation({ active, setActive }) {
+  return (
+    <div className="navigation">
+      <div className="user-con">
+        <img className="img" src={avatar} alt="" />
+        <div className="text">
+          <h2>Mike</h2>
+          <p>Your Money</p>
+        </div>
+      </div>
+      <ul className="menu-items">
+        {menuItems.map((item) => {
+          return (
+            <li
+              key={item.id}
+              onClick={() => setActive(item.id)}
+              className={active === item.id ? "active" : ""}>
+              {item.icon}
+              <span>{item.title}</span>
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className="bottom-nav">
+        <li>{signout} Sign Out</li>
+      </div>
+    </div>
+  );
 }
 
 export default Navigation;
