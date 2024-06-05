7ae85a62bafde8423600d0e5c85c0f0fa6278a28
Sourav <ksourav662@gmail.com>
2024-06-05 18:41:47 +0530

    add Background animation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.scss";
+import BgAnimation from "./components/bgAnimation/BgAnimation";
 function App() {
   return (
     <div className="app-container">
+      <BgAnimation />
       <main className="app-main">Hello</main>
     </div>
   );
---
+++ Frontend/src/components/bgAnimation/BgAnimation.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./BgAnimation.scss";
+function BgAnimation() {
+  return <div className="bg-animation"></div>;
+}
+
+export default BgAnimation;
