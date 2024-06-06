dceae88ed571f2ee4cebbc0a2b00086c9b174385
Sourav <ksourav662@gmail.com>
2024-06-06 17:12:15 +0530

    CreateContext All Done and add income functionality

--- Frontend/src/components/income/Income.jsx
+++ Frontend/src/components/income/Income.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Income.scss";
+import { useGlobalContext } from "../../context/GlobalContexts";
+import Form from "../form/Form";
+import IncomeItem from "../incomeItem/IncomeItem";
+
 function Income() {
+  const { incomes, getIncomes, deleteIncome, totalIncome } = useGlobalContext();
+  console.log(incomes);
+
+  useEffect(() => {
+    getIncomes();
+  }, []);
+
   return (
     <div className="income-container">
-      <div className="inner-income-container">Income</div>
+      <div className="inner-income-container">
+        <h1>Income</h1>
+        <h2 className="total-income">
+          Total Income: <span>${totalIncome()}</span>
+        </h2>
+        <div className="income-content">
+          <div className="form-container">
+            <Form />
+          </div>
+          <div className="incomes">
+            {incomes.map((income) => {
+              const { _id, title, amount, date, category, description, type } =
+                income;
+              return (
+                <IncomeItem
+                  key={_id}
+                  id={_id}
+                  title={title}
+                  description={description}
+                  amount={amount}
+                  date={date}
+                  type={type}
+                  category={category}
+                  deleteItem={deleteIncome}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
+import { GlobalProvider } from "./context/GlobalContexts.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GlobalProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GlobalProvider>
 );
